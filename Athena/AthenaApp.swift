ba2f96974998de50a01736082cbcabcdf4ea59ee
sachhabria <sach.chhabria@gmail.com>
2024-02-11 14:52:31 -0800

    Initial Commit

---
+++ Athena/AthenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaApp.swift
+//  Athena
+//
+//  Created by Sachin Chhabria on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AthenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
